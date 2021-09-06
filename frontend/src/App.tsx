3f4eecb53254d692f441ce694706b9787444e3fd
0dnammoc <vini.e@outlook.com>
2021-09-06 19:27:27 -0300

    Reajuste DataTable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,29 +9,28 @@ import { Fragment } from "react";
 function App() {
   return (
     <>
-    <NavBar />
-    <div className="container">
-      <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-      
-      <div className="row px-3">
-        <div className="cols-sm-6">
-          <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
-          <BarChart />
+      <NavBar />
+        <div className="container">
+          <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+          
+          <div className="row px-3">
+            <div className="cols-sm-6">
+              <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+              <BarChart />
+            </div>
+          <div className="cols-sm-6">
+              <h5 className="text-center text-secondary">Vendas</h5>
+              <DonutChart />
+            </div>
+          </div>
+          
+          <div className="py-3">
+            <h2 className="text-primary text-center">Todas as Vendas</h2>
+          </div>
+          <DataTable />
+
         </div>
-        <div className="cols-sm-6">
-          <h5 className="text-center text-secondary">Vendas</h5>
-          <DonutChart />
-        </div>
-      </div>
-
-    </div>
-
-    <div className="py-3">
-      <h2 className="text-primary text-center">Todas as Vendas</h2>
-    </div>
-
-    <DataTable />
-    <Footer />
+      <Footer />
     </>
   );
 }
